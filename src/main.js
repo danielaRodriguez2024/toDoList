c5b43b1fff2ab5985d1d6d19dd14cfbf154df931
Daniela Rodríguez <daniela.rodriguez2024@hilasal.superate.org.sv>
2023-10-27 07:59:26 -0600

    :fire: Problems with updates have been fixed

--- src/main.js
+++ src/main.js
@@ -22,3 +22,29 @@ function addTask() {
         taskInput.value = '';
     }
 }
+
+// Función para mover una tarea completada a la lista de tareas completadas
+function completeTask(index) {
+    const completedTask = tasks.splice(index, 1)[0];
+    completedTasks.push(completedTask);
+    renderTasks();
+}
+
+// Función para renderizar las tareas en las listas de pendientes y completadas
+function renderTasks() {
+    const taskList = document.getElementById('taskList');
+    const completedTaskList = document.getElementById('completedTaskList');
+
+    taskList.innerHTML = '';
+    completedTaskList.innerHTML = '';
+
+    tasks.forEach((task, index) => {
+        const taskItem = createTaskItem(task, index, 'Pendiente');
+        taskList.appendChild(taskItem);
+    });
+
+    completedTasks.forEach((task, index) => {
+        const taskItem = createTaskItem(task, index, 'Completada');
+        completedTaskList.appendChild(taskItem);
+    });
+}
